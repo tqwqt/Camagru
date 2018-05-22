5f7d13d8d8f7a10a0d7b3dcb7c4d90d906a0bccc
Vladyslav HAVRYLENKO <vhavryle@e1r4p5.unit.ua>
2018-05-22 16:47:05 +0300

    2

--- components/Router.php
+++ components/Router.php
@@ -15,11 +15,38 @@ class Router
         $routsPath = ROOT.'/config/routes.php';
         $this->routes = include($routsPath);
     }
+
+    private function getUri()
+    {
+        $uri = 0;
+        if (!empty($_SERVER['REQUEST_URI']))
+        {
+            $req_ur = $_SERVER['REQUEST_URI'];
+            $pos = strpos($req_ur, '.') + 4;
+            $req_ur = substr($req_ur, $pos);
+            $uri = trim($req_ur, '/');
+        }
+        return $uri;
+    }
     public function run()
     {
-        if (!empty($_SERVER['REQUEST_URI']))
-            $uri = trim($_SERVER['REQUEST_URI'], '/');
 
-        echo $uri;
+
+        $uri =  $this->getUri();
+        foreach ($this->routes as $pattern => $path)
+        {
+            //echo "pattern = $pattern, uri = $uri <br>";
+            if (preg_match("~^$pattern$~", $uri))
+            {
+                $exp = explode('/', $path);
+                $conName = array_shift($exp).'Controler';
+                $conName = ucfirst($conName);
+                echo $conName;
+                $actionName = array_shift($exp);
+                $actionName = 'action'.ucfirst($actionName);
+                echo "<br>";
+                echo  $actionName;
+            }
+        }
     }
 }
